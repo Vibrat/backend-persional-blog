01af7c7ab8ba2aa3ab7b3e386e887ad095b73a3e
Vibrat <nguyenhoanglam229@gmail.com>
2019-06-08 09:33:17 +0700

    working 06082019

--- source/api/controller/get/menuController.php
+++ source/api/controller/get/menuController.php
@@ -24,20 +24,9 @@ class MenuController extends Controller {
     public function isAlive() {
         $data = $this->http->data('PUT');
         
-        ## parse data from HTTP Header
-        $raw = array_shift($data);
-        $str_lines = preg_split("/\\r\\n----------------------------\d*-{0,2}\\r\\n(Content-Disposition: form-data; ){0,}/", $raw);
-        foreach($str_lines as $line) {
-
-            [$key, $value] = preg_split("/\\r\\n\\r\\n/", $line);
-            preg_match("/\"(.*?)\"/", $key, $key);
-            $data[$key] = $value;
-        }
-        
         $this->json->sendBack([
             'isAlive' => true,
             'data' => $data,
-            'raw'  =>  $str_lines
         ]);
     }
 
--- source/system/library/http/http.php
+++ source/system/library/http/http.php
@@ -13,27 +13,14 @@ class DataSubmit {
                 break;
             case 'PUT':
                 $this->data['GET']  = $_GET;
-                parse_str(
-                    file_get_contents(
-                        'php://input', 
-                        false , 
-                        null, 
-                        0, 
-                        $_SERVER['CONTENT_LENGTH'] ), 
-                    $this->data['PUT']);
+                $this->data['PUT']  = $this->parse_data(); 
+                break;
             case 'GET':
                 $this->data['GET'] = $_GET;   
                 break; 
             case 'DELETE':
                 $this->data['GET'] = $_GET;
-                parse_str(
-                    file_get_contents(
-                        'php://input', 
-                        false , 
-                        null, 
-                        0, 
-                        $_SERVER['CONTENT_LENGTH'] ), 
-                    $this->data['DELETE']);    
+                $this->data['DELETE']  = $this->parse_data(); 
                 break;         
         }
     }
@@ -46,4 +33,32 @@ class DataSubmit {
         return $_SERVER['REQUEST_METHOD'];
     }
 
+    /**
+     * Parse response getting from HTTP Header which supports form-data type
+     * This method only need to run in method DELETE and PUT 
+     * 
+     * 
+     * @important this does not support file type yet
+     */
+    private function parse_data() {
+        $data = [];
+        ## parse data from HTTP Header
+        parse_str(file_get_contents('php://input', false , null, 0, $_SERVER['CONTENT_LENGTH'] ), $data);
+        var_dump($data);
+        $data_lines = preg_split("/\\r\\n----------------------------\d*-{0,2}\\r\\n(Content-Disposition: form-data; ){0,}/", array_shift($data));
+        foreach($data_lines as $line) {
+            if (!empty($line)) {
+                $headers = [];
+                [$key, $value] = preg_split("/\\r\\n\\r\\n/", $line);
+                // preg_match_all('/(?:")([a-zA-Z\s\.\d]*?)(?:")/', $key, $key);
+                preg_match_all("/(?:;?)([a-zA-Z\s\.\d\"\/=:-]*)(?:;*|(?:\\r\\n)?)/", $key, $headers);
+                if (!empty($headers)) {
+
+                }
+            }
+        }
+
+        return $data;
+    }
+    
 }
